f1aff3d09ebcfa2e3f9b7d22b6579dd73d822d32
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-05-07 23:44:54 +0600

    react-firebase-hooks create

--- src/Component/Login/Register/Register.js
+++ src/Component/Login/Register/Register.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init'
 import './Register.css'
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+      
 
     const navigate =useNavigate();
 
@@ -11,6 +21,10 @@ const Register = () => {
 
     const handleRegister = async (event) =>{
         event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='register-form'>
