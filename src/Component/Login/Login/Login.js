89358c36d08f972e85e1201c432653e1aeef6663
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-05-07 20:55:35 +0600

    register from create

--- src/Component/Login/Login/Login.js
+++ src/Component/Login/Login/Login.js
@@ -1,14 +1,25 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password)
+    }
+    const navigateRegister = event => {
+        navigate('/register')
+    }
     return (
         <div className='container w-50 mx-auto'>
         <h1 className='text-primary text-center mt-2'> Please Login </h1>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control ref={emailRef}  type="email" placeholder="Enter email" required />
@@ -24,7 +35,7 @@ const Login = () => {
                 Login
             </Button>
         </Form>
-        <p>If you are the first to visit our website, <Link to="/register" className='text-danger pe-auto text-decoration-none'> Please Register </Link></p>
+        <p>If you are the first to visit our website, <Link to="/register" className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}> Please Register </Link></p>
         <p>Forget Password <button className='text-primary pe-auto text-decoration-none btn btn-link' > Please Reset Password </button></p>
        
         
--- src/Component/Login/Register/Register.js
+++ src/Component/Login/Register/Register.js
@@ -1,10 +1,38 @@
 import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 const Register = () => {
+
+    const navigate =useNavigate();
+
+    const navigateLogin = () =>{
+         navigate('/login')
+    }
+
+    const handleRegister = async (event) =>{
+        event.preventDefault();
+    }
     return (
-        <div>
-            rigister
-        </div>
+        <div className='register-form'>
+        <h1 style={{textAlign:'center'}}>  Please Register </h1>
+        <form onSubmit={handleRegister}>
+            <input type="text" name='name' id='' placeholder='Your Name' />
+            <input type="email" name='email' id='' placeholder='Your Email' required/>
+            <input type="password" name='password' id='' placeholder='Your Password' required/>
+            <div style={{textAlign:'center'}}>
+            <input type="checkbox" name="terms" id="terms" />
+            <lebel className={'ps-1 text-danger'} htmlFor="terms" >Accept Terms and Conditions</lebel>
+            </div>
+            
+           
+            <input 
+           
+            className='w-50 mx-auto-btn submit-custom mt-3'
+             type="submit" value="Register" />
+        </form>
+        <p style={{textAlign:'center'}}>All ready have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}> Please Login </Link></p>
+        
+       </div>
     );
 };
 
